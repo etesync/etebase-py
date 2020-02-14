20421d4ef6c743d6eba3161487bdc73e985679d5
Tom Hacohen <tom@stosb.com>
2020-02-14 15:57:47 +0200

    Fix licensing headers to be LGPL.
    Was a mistake when adding them.
    
    Fixes #18.

--- etesync/pim.py
+++ etesync/pim.py
@@ -1,8 +1,8 @@
 # Copyright © 2017 Tom Hacohen
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3.
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, version 3.
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
