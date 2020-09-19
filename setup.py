5087339a8c08d7c507f119343423a1bdcd9163d6
Tom Hacohen <tom@stosb.com>
2020-09-19 15:35:53 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="etebase",
-    version="0.20.2",
+    version="0.21.0",
     rust_extensions=[RustExtension("etebase.etebase_python", binding=Binding.RustCPython)],
     packages=["etebase"],
     author='Tom Hacohen',
